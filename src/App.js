147706855f28f0765b1926dbded7da255d6b8db4
Stephen Rayner <stephen.rayner@marmalade.co.uk>
2019-05-14 18:47:34 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Form from "./components/form";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Congratulations you have a react application.</p>
+        <Button>Click me</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Form />
       </header>
     </div>
   );
---
+++ src/components/form.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Form = props => {
+  return (
+    <form>
+      <div className="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Enter email"
+        />
+        <small id="emailHelp" className="form-text text-muted">
+          We'll never share your email with anyone else.
+        </small>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
